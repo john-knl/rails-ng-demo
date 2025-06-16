9bb3db368cbf7aa82c35a292e648f6ddb47f5ec2
John Le <kh.nguyen.le@gmail.com>
2025-06-16 19:30:02 -0400

    add render to allowed hosts

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.hosts << ".rngwidgets.com"
+  config.hosts << ".onrender.com"
 
   config.host_authorization = {
     exclude: ->(request) { request.path =~ /health/ }
