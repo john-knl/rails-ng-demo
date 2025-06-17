9e6a3ae9e802e06cf32c3ca109821f1edefa76a0
John Le <kh.nguyen.le@gmail.com>
2025-06-17 13:25:23 -0400

    Add additional allowed request origin for Action Cable

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = "/cable"
   config.action_cable.url = "wss://api.rngwidgets.com/cable"
-  config.action_cable.allowed_request_origins = ['https://www.rngwidgets.com']
+  config.action_cable.allowed_request_origins = ['https://www.rngwidgets.com', 'https://api.rngwidgets.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
