157d206476aec9f5b10047902138df8b4359e20b
John Le <kh.nguyen.le@gmail.com>
2025-06-17 13:00:41 -0400

    Add middleware for Action Cable support in API mode

--- api/config/application.rb
+++ api/config/application.rb
@@ -23,5 +23,10 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Add middleware needed for Action Cable (WebSockets) in API mode
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::Flash
   end
 end
