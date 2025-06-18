b2f9afebc6ee925e0dcdf8aaeecb301a35b0dbcb
John Le <kh.nguyen.le@gmail.com>
2025-06-18 12:01:17 -0400

    Move ActionCable mount to the top of routes file

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
+mount ActionCable.server => '/cable'
+
 Rails.application.routes.draw do
-  mount ActionCable.server => '/cable'
 
   constraints subdomain: 'api' do
     resources :widgets
