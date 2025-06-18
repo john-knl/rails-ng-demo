991cf671ff16b5271fd1cbca5e4f1e6f0cacdd7d
John Le <kh.nguyen.le@gmail.com>
2025-06-18 10:43:34 -0400

    Remove production server start command from entrypoint script

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -5,10 +5,6 @@ bundle exec rails db:prepare
 
 bundle exec rails db:seed
 
-if [ "$RAILS_ENV" = "production" ]; then
-  bundle exec rails s -e production;
-fi
-
 if [ "$RALS_ENV" = "test" ]; then
   bundle exec rails s -b "0.0.0.0" -e test &
 fi
